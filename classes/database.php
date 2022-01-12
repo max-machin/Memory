18ea21a5538bcc8bf692522950a033d2ec35742a
max-machin <machin.max@lapalteforme.io>
2022-01-12 15:35:34 +0100

    création du fichier classe avec la connexion à la BDD en PDO et la création du fichier class carte

---
+++ classes/database.php
@@ -0,0 +1,24 @@
+<?php
+class database
+{
+
+    public function __construct()
+    {
+        try 
+        {
+            $conn = new PDO("mysql:host=localhost;dbname=memory", "root", "");
+            //Definition du mode d'erreur de PDO sur expetion
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connexion réussie";
+        //On capture l'exeption qui est executée en cas de probléme de connexion à la bdd puis on affiche les informations relatives
+        } 
+        catch (PDOException $e)
+        {
+            die(print('Erreur :! ' . $e->getMessage()));  
+        }
+        //Fermeture de la connexion bdd 
+        $conn = null;
+    }
+}
+
+?>
